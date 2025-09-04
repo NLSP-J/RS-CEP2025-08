5330144ec14639ba6cefe4f2d73e17e4e6e78589
NLSP-J <justin@nullspace.co>
2025-09-04 15:46:14 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,110 @@
+import pygame as pg
+import random, time
+import asyncio
+pg.init()
+clock = pg.time.Clock()
+
+white = (255, 255, 255)
+
+win_width = 700
+win_height = 700
+screen = pg.display.set_mode((win_width, win_height))
+pg.display.set_caption('Penguin Mission II')
+
+font = pg.font.Font(None, 30)
+speed = 20
+score = 0
+running = True
+lives = 3
+
+player_size = 50
+player_pos = [win_width / 2, win_height - player_size]  # 400, 600-40
+player_image = pg.image.load('./assets/images/penguinempire.png')
+player_image = pg.transform.scale(player_image, (player_size, player_size))  # 40,40
+
+obj_size = 40
+obj_data = []     
+obj = pg.image.load('./assets/images/J.png')
+obj = pg.transform.scale(obj, (obj_size, obj_size))
+
+bg_image = pg.image.load('./assets/images/glorious-aurora-borealis-over-antarctica-landscape-ai-generative-generated-timeless-image-spectacular-norden-lights-283507349.webp')
+bg_image = pg.transform.scale(bg_image, (win_width, win_height))
+
+
+def create_object(obj_data):
+    if len(obj_data) < 10 and random.random() < 0.1:            
+        x = random.randint(0, win_width - obj_size)
+        y = 0                                         
+        obj_data.append([x, y, obj])
+
+
+def update_objects(obj_data):
+    global score
+
+    for object in obj_data:
+        x, y, image_data = object
+        if y < win_height:
+            y += speed
+            object[1] = y
+            screen.blit(image_data, (x, y))
+        else:
+            obj_data.remove(object)
+            score += 1
+
+
+def collision_check(obj_data, player_pos):
+    global running, lives
+    for object in obj_data:
+        x, y, image_data = object
+        player_x, player_y = player_pos[0], player_pos[1]
+        obj_rect = pg.Rect(x, y, obj_size, obj_size)
+        player_rect = pg.Rect(player_x, player_y, player_size, player_size)
+        if player_rect.colliderect(obj_rect):
+            lives -= 1
+            obj_data.remove(object)
+            if lives == 0:
+                time.sleep(2)
+                running = False
+                break
+
+async def main():
+    global running, player_pos
+
+    while running:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+
+            if event.type == pg.KEYDOWN:
+                x, y = player_pos[0], player_pos[1]
+                if event.key == pg.K_LEFT:
+                    x -= 20
+                elif event.key == pg.K_RIGHT:
+                    x += 20
+                player_pos = [x, y]
+
+        screen.blit(bg_image, (0, 0))
+        screen.blit(player_image, (player_pos[0], player_pos[1]))
+
+        text = f'Streak: {score}'
+        text = font.render(text, 10, white)
+        screen.blit(text, (win_width - 200, win_height - 40))
+
+        text = f'Lives: {lives}'
+        text = font.render(text, 10, white)
+        screen.blit(text, (win_width - 200, win_height - 80))
+
+
+        text = f'Survive,Penguin!'
+        text = font.render(text, 10, white)
+        screen.blit(text, (win_width - 200, win_height - 60))    
+
+        create_object(obj_data)
+        update_objects(obj_data)
+        collision_check(obj_data, player_pos)
+
+        clock.tick(30)
+        pg.display.flip()
+        await asyncio.sleep(0)
+
+asyncio.run(main())
